baa0087a5bc2775f16e6194086a2112ac8a8a7c5
Adil <christie_m@mail.ru>
2024-01-30 12:46:38 +0300

    FS_HW5

--- FS_HW5/src/main/java/ru/fsv67/models/Book.java
+++ FS_HW5/src/main/java/ru/fsv67/models/Book.java
@@ -1,13 +1,22 @@
 package ru.fsv67.models;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
+@Entity
 @Data
 public class Book {
     private static long idCounter = 1L;
 
-    private final long id;
-    private final String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Book() {
+    }
 
     public Book(String name) {
         this.id = idCounter++;
