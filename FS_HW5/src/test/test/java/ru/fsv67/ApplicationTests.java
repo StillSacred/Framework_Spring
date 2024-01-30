2c63462dd20e263b6674be070ddad45e8fd93fc2
Adil <christie_m@mail.ru>
2024-01-30 12:29:13 +0300

    FS_HW5

---
+++ FS_HW5/src/test/test/java/ru/fsv67/ApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.fsv67;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
